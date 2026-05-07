533de564eb2934e2e7df0049b1b55a33b40fbffc
ashxyz18 <ashxyz18@users.noreply.github.com>
2026-05-07 21:29:21 +0600

    Remove functions and scripts directories

--- functions/[[path]].ts
+++
@@ -1,9 +0,0 @@
-// Cloudflare Pages catch-all route for Next.js
-// This passes all requests to the Next.js application
-
-export const onRequest: PagesFunction = async (context) => {
-  const url = new URL(context.request.url);
-  
-  // Pass the request to Next.js
-  return fetch(context.request);
-};
