d93cee94f5898b24acb7783dd6767a08f08a01df
ashxyz18 <ashxyz18@users.noreply.github.com>
2026-05-12 21:25:55 +0600

    Update database seed and dependencies

--- backend/database/src/seed.ts
+++ backend/database/src/seed.ts
@@ -4,12 +4,12 @@ async function main() {
   const admin = await prisma.user.upsert({
     where: { email: "admin@bdesh.shop" },
     update: {
-      password: "$2b$12$FSu0ZmEJoKlGPeceRZ1g8O9HkSGB71I8WR29lgDOBW6Cu6B7GU/lK",
+      password: "$2b$12$kVlknKCNEr/gjGiqQwsHKu3e5EdeW/GtytwjDoWzkrzbMePos.h/W",
     },
     create: {
       email: "admin@bdesh.shop",
       name: "System Admin",
-      password: "$2b$12$FSu0ZmEJoKlGPeceRZ1g8O9HkSGB71I8WR29lgDOBW6Cu6B7GU/lK",
+      password: "$2b$12$kVlknKCNEr/gjGiqQwsHKu3e5EdeW/GtytwjDoWzkrzbMePos.h/W",
       role: "ADMIN",
     },
   });
